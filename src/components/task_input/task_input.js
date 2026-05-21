5d8701674f1f212177785766988830d7a8efd843
Maria Eduarda <medeiroslima.me@gmail.com>
2026-05-21 00:09:21 -0300

    feat: added sound and visual feedback when the character limit is reached

--- src/components/task_input/task_input.js
+++ src/components/task_input/task_input.js
@@ -1,15 +1,48 @@
+import { useState } from "react";
 import "./task_input.css";
 
+const charLimit = 100;
+
+const errorSound = new Audio("/Windows Default Beep.wav");
+
 export default function TaskInput({ value, onChange, placeholder, onKeyDown }) {
+	const [showWarning, setShowWarning] = useState(false);
+
+	const handleInternalKeyDown = (e) => {
+		if (
+			e.key !== "Backspace" &&
+			e.key !== "Delete" &&
+			e.key !== "Enter" &&
+			value.length === charLimit
+		) {
+			setShowWarning(true);
+			errorSound.currentTime = 0;
+			errorSound
+				.play()
+				.catch((e) => console.log("The browser has blocked the sound: ", e));
+		}
+		onKeyDown(e);
+	};
+
+	const handleInternalChange = (e) => {
+		setShowWarning(false);
+		onChange(e);
+	};
+
 	return (
 		<>
 			<input
 				type="text"
-				className="task-input"
+				className={
+					showWarning === true && value.length === charLimit
+						? "task-input char-limit-reached"
+						: "task-input"
+				}
+				maxLength={charLimit}
 				value={value}
-				onChange={onChange}
+				onChange={handleInternalChange}
 				placeholder={placeholder}
-				onKeyDown={onKeyDown}
+				onKeyDown={handleInternalKeyDown}
 			/>
 		</>
 	);
